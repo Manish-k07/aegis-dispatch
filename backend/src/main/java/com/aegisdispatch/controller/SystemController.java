e7565890b98ee5aec9c2d484ae94415ce36c4002
MANISH K S <manishkshankar2@gmail.com>
2026-09-23 10:10:19 +0530

    security: remove wildcard API CORS

--- backend/src/main/java/com/aegisdispatch/controller/SystemController.java
+++ backend/src/main/java/com/aegisdispatch/controller/SystemController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/system")
-@CrossOrigin(origins = "*")
+
 public class SystemController {
 
     private final CadFileService fileService;
